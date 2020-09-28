f72b3afb57b96c90f2181f3e600de8a973d91486
DaisukeKameyama <31030@ojs.ac.jp>
2020-09-28 15:26:02 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chapter3 {
+    class Program {
+        static void Main(string[] args) {
+            var names = new List<string> {
+                "Tokyo","New Delhi","Bangkok","London","Paris","Berlin","Canbeerra","HongKong",
+            };
+        }
+    }   
+}
