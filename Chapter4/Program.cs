6d9d753205ca97baf5263d90e3e9229654e877dd
DaisukeKameyama <31030@ojs.ac.jp>
2020-10-02 15:01:17 +0900

    問題4.2

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -1,17 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Chapter4 {
     class Program {
         static void Main(string[] args) {
-            for(int i = 0; i < 5; i++) {
-                var array = new YearMonth[5];
-                array[i] = new YearMonth(2001, 8);
+            //4.2.1
+            var yearMonths = new YearMonth[] {
+               new YearMonth(1980,1),
+               new YearMonth(1990,1),
+               new YearMonth(2000,1),
+               new YearMonth(2010,1),
+               new YearMonth(2020,1),
+           };
+            //4.2.2
+            foreach(var item in yearMonths) {
+                Console.WriteLine(item);
             }
-            
+            Console.WriteLine("--------------");
+
+            //4.2.3
+            Console.WriteLine(FindFirst21C(yearMonths));
+            Console.WriteLine("--------------");
+
+            //4.2.4
+            Ex2_4(yearMonths);
+            Console.WriteLine("--------------");
+
+            //4.2.5
+            Ex2_5(yearMonths);
+            Console.WriteLine("--------------");
+        }
+
+        //4.2.3のメソッド
+        static YearMonth FindFirst21C(YearMonth[] yearMonths) {
+            foreach(var item in yearMonths) {
+                if(2001 <= item.Year && item.Year <= 2100) {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+        private static void Ex2_4(YearMonth[] yms) {
+            var yearmonth = FindFirst21C(yms);
+            var s = yearmonth == null ? "21世紀のデータはありません" : yearmonth.ToString();
+            Console.WriteLine(s);
+        }
+
+        private static void Ex2_5(YearMonth[] yms) {
+            var array = yms.Select(s => s.AddMonth()).ToArray();
+            foreach(var item in array) {
+                Console.WriteLine(item);
+            }
         }
     }
 }
