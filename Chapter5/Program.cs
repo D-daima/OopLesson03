54eaaeaebdf4b304366e6459002855b5e8b20119
DaisukeKameyama <31030@ojs.ac.jp>
2020-10-05 14:58:54 +0900

    第5章演習問題　

--- Chapter5/Program.cs
+++ Chapter5/Program.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 namespace Chapter5 {
     class Program {
         static void Main(string[] args) {
-            //5.1
+            #region//5.1
             Console.Write("文字列入力：");
             var Fstr = Console.ReadLine();
             Console.Write("文字列入力：");
@@ -22,8 +22,9 @@ namespace Chapter5 {
             }
 
             Console.WriteLine("----------");
+            #endregion
 
-            //5.2
+            #region//5.2
             Console.WriteLine("数値入力：");
             var line = Console.ReadLine();
             int num;
@@ -34,34 +35,60 @@ namespace Chapter5 {
             } else {
                 Console.WriteLine("数値文字列ではありません");
             }
+            #endregion
 
+            #region 5.3
             //5.3.1
-            var Ex5_3 = "Jackdaws love my big sphinx of quartz";
-            Console.WriteLine($"{Ex5_3.Count()}文字");
+            var text = "Jackdaws love my big sphinx of quartz";      
+            Console.WriteLine($"空白{text.Count(c => c == ' ')}文字");
             Console.WriteLine("------------");
 
             //5.3.2
-            var replace = Ex5_3.Replace("big", "small");
+            var replace = text.Replace("big", "small");
             Console.WriteLine(replace);
             Console.WriteLine("------------");
 
             //5.3.3
-            string[] words = Ex5_3.Split(' ');
+            string[] words = text.Split(' ');
+            //int n = text.Split(' ').Count()
             Console.WriteLine($"{words.Length}単語");
             Console.WriteLine("------------");
 
             //5.3.4
             words.Where(s => s.Length <= 4).ToList().ForEach(Console.WriteLine);
             Console.WriteLine("------------");
+            
 
             //5.3.5
-            var sb = new StringBuilder();
-            foreach(var word in words) {
-                sb.Append(word);
+            if(words.Length > 0) {
+                var sb = new StringBuilder(words[0]);
+                foreach(var word in words.Skip(1)) {
+                    sb.Append(' ');
+                    sb.Append(word);
+                }
+                Console.WriteLine(sb.ToString());
             }
-            var text = sb.ToString();
-            Console.WriteLine(text);
-            
+            Console.WriteLine("------------");
+            #endregion
+
+            //5.4
+            var target = "Novelist=谷崎潤一郎;BestWork=春琴沙;Born=1886";            
+            foreach(var item in target.Split(';')) {
+                var wd = item.Split('=');
+                Console.WriteLine("{0}:{1}", Tojapanese(wd[0]), wd[1]);
+            }
+        }
+        static string Tojapanese(string key) {
+            switch(key) {
+                case "Novelist":
+                    return "作家　";
+                case "BestWork":
+                    return "代表作";
+                case "Born":
+                    return "誕生年";
+                default:
+                    return "        ";
+            }
         }
     }
 }
