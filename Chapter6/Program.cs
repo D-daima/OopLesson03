6d79afed83fb50ed99bfa19a83ee9fcf39204124
DaisukeKameyama <31030@ojs.ac.jp>
2020-10-15 10:02:17 +0900

    P196まで

--- Chapter6/Program.cs
+++ Chapter6/Program.cs
@@ -41,8 +41,9 @@ namespace Chapter6 {
             Console.WriteLine("\n\n------------");
             #endregion
 
+            #region 6.2
             var books = new List<Book> {
-                new Book { Title = "C#プログラミングの新常識", Price = 3800, Pages = 378 },
+                new Book { Title = "C#プログラミングの新常識「C#」", Price = 3800, Pages = 378 },
                 new Book { Title = "ラムダ式とLINQの極意", Price = 2500, Pages = 312 },
                 new Book { Title = "ワンダフル・C#ライフ", Price = 2900, Pages = 385 },
                 new Book { Title = "一人で学ぶ並列処理プログラミング", Price = 4800, Pages = 464 },
@@ -51,50 +52,66 @@ namespace Chapter6 {
                 new Book { Title = "楽しいC#プログラミング教室", Price = 2540, Pages = 348 },
             };
 
-            //6.2.1
-            Console.WriteLine("問題6.2.1");
-            var book = books.Where(x => x.Title == "ワンダフル・C#ライフ");
-            foreach(var item in book) {
-                Console.WriteLine($"金額：{item.Price}");
-                Console.WriteLine($"ページ：{item.Pages}");
+            ////6.2.1
+            //Console.WriteLine("問題6.2.1");
+            //var book = books.FirstOrDefault(x => x.Title == "ワンダフル・C#ライフ");
+            //if(book != null) {
+            //    Console.WriteLine($"{book.Price}円:{book.Pages}ページ");
+            //}
+            //Console.WriteLine("\n------------");
+
+            ////6.2.2
+            //Console.WriteLine("問題6.2.2");
+            //int count = books.Count(x => x.Title.Contains("C#"));
+            //Console.WriteLine($"{count}冊");
+            //Console.WriteLine("\n------------");
+
+            ////6.2.3
+            //Console.WriteLine("問題6.2.3");
+            //var avg = books.Where(x => x.Title.Contains("C#")).Average(x => x.Pages);
+            //Console.WriteLine($"タイトルにC#が含まれる書籍の平均ページ：{avg}ページ");
+            //Console.WriteLine("\n------------");
+
+            ////6.2.4
+            //Console.WriteLine("問題6.2.4");
+            //var obj =  books.FirstOrDefault(x => x.Price >= 4000);
+            //if(obj != null) {
+            //    Console.WriteLine($"タイトル：{obj.Title}");
+            //}           
+            //Console.WriteLine("\n------------");
+
+            ////6.2.5
+            //Console.WriteLine("問題6.2.5");
+            //var pages = books.Where(x => x.Price < 4000).Max(x => x.Pages);
+            //Console.WriteLine($"最大{pages}ページ");
+            //Console.WriteLine("\n------------");
+
+            ////6.2.6
+            //Console.WriteLine("問題6.2.6");
+            //var odPrices = books.Where(x => x.Pages >= 400).OrderByDescending(x => x.Price);
+            //foreach(var odPrice in odPrices) {
+            //    Console.WriteLine($"タイトル：{odPrice.Title}");
+            //    Console.WriteLine($"価格：{odPrice.Price}");
+            //}
+            //Console.WriteLine("\n------------");
+
+            ////6.2.7
+            //Console.WriteLine("問題6.2.7");
+            //var selected = books.Where(x => x.Title.Contains("C#") && x.Pages <= 500);
+            //foreach(var select in selected) {
+            //    Console.WriteLine($"タイトル：{select.Title}");
+            //}
+            #endregion
+
+            //すべての書籍から「C#」の文字がいくつあるかをカウントする
+            int count = 0;
+            foreach(var book in books) {
+                for(int i = 0; i < book.Title.Length - 1; i++) {
+                    if((book.Title[i] == 'C') && (book.Title[i + 1] == '#'))
+                        count++;
+                }                
             }
-            Console.WriteLine("\n------------");
-
-            //6.2.2
-            Console.WriteLine("問題6.2.2");
-            var count = books.Count(x => x.Title.Contains("C#"));
-            Console.WriteLine($"{count}冊");
-            Console.WriteLine("\n------------");
-
-            //6.2.3
-            Console.WriteLine("問題6.2.3");
-            var avg = books.Where(x => x.Title.Contains("C#")).Average(x => x.Pages);
-            Console.WriteLine($"タイトルにC#が含まれる書籍の平均ページ：{avg}ページ");
-            Console.WriteLine("\n------------");
-
-            //6.2.4
-            Console.WriteLine("問題6.2.4");
-            var obj =  books.FirstOrDefault(x => x.Price >= 4000);
-            Console.WriteLine($"タイトル：{obj.Title}");
-            Console.WriteLine("\n------------");
-
-            //6.2.5
-            Console.WriteLine("問題6.2.5");
-            Console.WriteLine($"最大{books.Where(x => x.Price < 4000).Max(x => x.Pages)}ページ");
-            Console.WriteLine("\n------------");
-
-            //6.2.6
-            Console.WriteLine("問題6.2.6");
-            var odPrices = books.Where(x => x.Pages >= 400).OrderByDescending(x => x.Price);
-            foreach(var odPrice in odPrices) {
-                Console.WriteLine($"タイトル：{odPrice.Title}");
-                Console.WriteLine($"価格：{odPrice.Price}");
-            }
-            Console.WriteLine("\n------------");
-
-            //6.2.7
-            Console.WriteLine("問題6.2.7");
-            books.Where(x => x.Title.Contains("C#") && x.Pages <= 500).ToList().ForEach(s => Console.WriteLine(s.Title));
+            Console.WriteLine($"文字列「C#」の個数は{count}です。");
         }
     }
 }
---
+++ Chapter7/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter7 {
+    class Program {
+        static void Main(string[] args) {
+            #region
+            //var employeeDict = new Dictionary<int, Employee> {
+            //    {100,new Employee(100,"清水遼久") },
+            //    {112,new Employee(112,"芹沢洋和") },
+            //    {125,new Employee(125,"岩瀬奈央子") },
+            //};
+            //var employees = employeeDict.Where(emp => emp.Value.Id % 2 == 0).ToList();
+            //foreach(var item in employees) {
+            //    Console.WriteLine($"{item.Value.Id} = {item.Value.Name}");
+            //}
+            //リスト
+            //List<Employee> employee = new List<Employee>() {
+            //    new Employee(100,"清水遼久"),
+            //    new Employee(112,"芹沢洋和"),
+            //    new Employee(125,"岩瀬奈央子"),
+            //    new Employee(143,"山田太郎"),
+            //    new Employee(148,"池田太郎"),
+            //    new Employee(152,"高田太郎"),
+            //    new Employee(155,"石川太郎"),
+            //    new Employee(161,"中沢太郎"),
+            //};
+            ////Idが偶数のみディクショナリに変換する
+            //var employeeDict = employee.Where(emp => emp.Id % 2 == 0).ToDictionary(emp => emp.Id);
+            //foreach(var item in employeeDict) {
+            //    Console.WriteLine($"{item.Value.Id} {item.Value.Name}");
+            //}
+            //Console.WriteLine(num);
+            #endregion
+
+            //var lines = File.ReadAllLines("sample.txt");
+            //var we = new WordsExtractor(lines);
+            //foreach(var word in we.Extract()) {
+            //    Console.WriteLine(word);
+            //}
+            DuplicateKeySample();
+        }
+        
+        static public void DuplicateKeySample() {
+            // ディクショナリの初期化
+            var dict = new Dictionary<string, List<string>>() {
+               { "PC", new List<string> { "パーソナル コンピュータ", "プログラム カウンタ", } },
+               { "CD", new List<string> { "コンパクト ディスク", "キャッシュ ディスペンサー", } },
+            };
+            
+            // ディクショナリに追加
+            var key = "EC";
+            var value = "電子商取引";
+            if(dict.ContainsKey(key)) {
+                dict[key].Add(value);
+            } else {
+                dict[key] = new List<string> { value };
+            }
+
+            // ディクショナリの内容を列挙
+            foreach(var item in dict) {
+                foreach(var term in item.Value) {
+                    Console.WriteLine("{0} : {1}", item.Key, term);
+                }
+            }
+        }
+    }
+}
