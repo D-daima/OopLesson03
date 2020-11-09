274e0898763d232272016bec48372e58cddb8b0c
DaisukeKameyama <31030@ojs.ac.jp>
2020-11-09 14:26:29 +0900

    設定情報による送信

--- SendMailApp/MainWindow.xaml.cs
+++ SendMailApp/MainWindow.xaml.cs
@@ -62,10 +62,10 @@ namespace SendMailApp {
                 }
 
                 Config cf = Config.GetInstance();
-                sc.Host = "smtp.gmail.com";//SMTPサーバーの設定
-                sc.Port = 587;
-                sc.EnableSsl = true;
-                sc.Credentials = new NetworkCredential("ojsinfosys01@gmail.com", "ojsInfosys2020");
+                sc.Host = cf.Smtp;//SMTPサーバーの設定
+                sc.Port = cf.Port;
+                sc.EnableSsl = cf.Ssl;
+                sc.Credentials = new NetworkCredential(cf.MailAddress, cf.PassWord);
                 //sc.Send(msg);//送信
                 sc.SendMailAsync(msg);
             } catch(Exception ex) {
